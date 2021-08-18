a0f33299b853424c7b9e504064dc29158491329c
Phu Doan <pdoan@safetrust.com>
2021-08-18 11:59:39 +0700

    init compose

--- app/src/main/ml/zedlabs/statetestcompose/MainApplication.kt
+++
@@ -1,8 +0,0 @@
-package ml.zedlabs.statetestcompose
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MainApplication: Application() {
-}
--- app/src/main/ml/zedlabs/statetestcompose/db/NoteDatabase.kt
+++
@@ -1,9 +0,0 @@
-package ml.zedlabs.statetestcompose.db
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Note::class], version = 2)
-abstract class NoteDatabase: RoomDatabase(){
-    abstract fun getNotesDao() : NotesDao
-}
